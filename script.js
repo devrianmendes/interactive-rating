76d0a46c9294f2b51230f2216e0d45a132dff4ea
devrianmendes <dev.rianmendes@outlook.com>
2022-05-29 17:12:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+const btn = document.querySelectorAll('.note-btn');
+const submit = document.querySelector('button');
+const box = document.querySelector('.inner-rate-box');
+const box2 = document.querySelector('.inner-submit-box');
+const chosen = document.querySelector('.chosen');
+
+function note(index) {
+  chosen.innerText = index + 1;
+}
+
+function handleActive(event, index) {
+  btn.forEach((btn) => {
+    btn.classList.remove('active')
+  })
+  event.target.classList.add('active');
+  note(index)
+}
+
+function handleSubmit() {
+  box.classList.add('submit');
+  box2.classList.add('submit');
+}
+
+btn.forEach((btn, index) => {
+  btn.addEventListener('click', function(event) {
+    handleActive(event, index);
+  })
+})
+
+submit.addEventListener('click', handleSubmit);
